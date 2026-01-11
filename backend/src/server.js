6874da1f13411850333bfe0ed3cb679055a22a2b
Shivansh Agarwal <shivanshagarwalup@gmail.com>
2026-01-11 18:17:21 +0530

    importing functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path"
 import cors from "cors"
-import serve from "inngest/express"
+import { serve } from "inngest/express";
 
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
