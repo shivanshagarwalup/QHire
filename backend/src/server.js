f3850eacf7e0fa816e688146db92f9efa69377ac
Shivansh Agarwal <shivanshagarwalup@gmail.com>
2026-01-12 00:02:56 +0530

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,55 +1,45 @@
 import express from "express";
-import path from "path";
-import cors from "cors";
+import path from "path"
+import cors from "cors"
 import { serve } from "inngest/express";
 
-import { ENV } from "./lib/env.js";
+import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
-const __dirname = path.resolve();
 
-// Middlewares
-app.use(express.json());
+const __dirname = path.resolve()
 
-// CORS logic
-if (ENV.NODE_ENV === "production") {
-  app.use(cors());
-} else {
-  app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-}
+//middlewares
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
-// API routes
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "successful by api" });
-});
+app.get("/health",(req,res) => {
+    res.status(200).json({msg:"succesful by api"})
+})
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "this is book endpoint" });
-});
+app.get("/books",(req,res) => {
+    res.status(200).json({msg:"this is book endpoint"})
+})
 
-// ---- STATIC FRONTEND SERVE ----
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if(ENV.NODE_ENV === "production") {
+    // Serve static files from the React build
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+    // Catch all remaining requests and return React's index.html
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
+
 
 const startServer = async () => {
   try {
     await connectDB();
-
-    // Render will inject PORT
-    const PORT = process.env.PORT || ENV.PORT || 3000;
-
-    app.listen(PORT, () =>
-      console.log(`Server is running on port: ${PORT}`)
-    );
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
