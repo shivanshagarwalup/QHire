bba48608f4ff091c896c6d6438085c9cda819e7f
Shivansh Agarwal <shivanshagarwalup@gmail.com>
2026-01-16 22:11:22 +0530

    fix prod frontend serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,40 +1,39 @@
 import express from "express";
-import path from "path"
-import cors from "cors"
+import path from "path";
+import cors from "cors";
 import { serve } from "inngest/express";
 
-import { ENV } from "./lib/env.js"
+
+import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
+
 const app = express();
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-//middlewares
-app.use(express.json())
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get("/health",(req,res) => {
-    res.status(200).json({msg:"succesful by api"})
-})
 
-app.get("/books",(req,res) => {
-    res.status(200).json({msg:"this is book endpoint"})
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
-if(ENV.NODE_ENV === "production") {
-    // Serve static files from the React build
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // Catch all remaining requests and return React's index.html
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    });
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-
 
 const startServer = async () => {
   try {
